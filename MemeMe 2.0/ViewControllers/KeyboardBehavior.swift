a9f95fc0fb205900c30010014ebff0f0d27a818e
evan barbour <evan.c.barbour@gmail.com>
2019-04-02 15:22:40 -0400

    MemeMe 2.0 updates

---
+++ MemeMe 2.0/ViewControllers/KeyboardBehavior.swift
@@ -0,0 +1,20 @@
+//
+//  KeyboardBehavior.swift
+//  Meme 1.0
+//
+//  Created by Evan Barbour on 3/31/19.
+//  Copyright © 2019 Evan Barbour. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class keyboardBehavior {
+    
+    var view: UIView
+    var topTextField: UITextField
+    var bottomTextField: UITextField
+    
+
+   
+}
---
+++ MemeMe 2.0/ViewControllers/MemeCell.swift
@@ -0,0 +1,21 @@
+//
+//  MemeCellViewController.swift
+//  Meme 1.0
+//
+//  Created by Evan Barbour on 3/26/19.
+//  Copyright © 2019 Evan Barbour. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class MemeCell: UITableViewCell {
+    
+        
+        @IBOutlet weak var memeImageView: UIImageView!
+        
+        @IBOutlet weak var memeImageLabel: UILabel!
+    }
+    
+
+
